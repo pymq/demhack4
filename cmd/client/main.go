bd3ddd363003a0638e6d6345b15fbf3cd62b0d51
GrigoryKrasnochub <42908925+GrigoryKrasnochub@users.noreply.github.com>
2022-06-05 20:11:58 +0300

    add tray for client app (#7)
    * add tray for client app, add .gitignore

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,136 +2,26 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"os"
+	"os/signal"
+	"syscall"
 
-	"filippo.io/age"
-	"github.com/knadh/koanf"
-	"github.com/knadh/koanf/parsers/json"
-	"github.com/knadh/koanf/providers/file"
-	"github.com/libp2p/go-yamux/v3"
-	"github.com/pymq/demhack4/config"
-	"github.com/pymq/demhack4/encoding"
-	"github.com/pymq/demhack4/icq"
-	"github.com/pymq/demhack4/socksproxy"
+	"github.com/pymq/demhack4/cmd/internal/client"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	k := koanf.New(".")
-	err := k.Load(file.Provider(config.ClientFilename), json.Parser())
-	if err != nil && !os.IsNotExist(err) {
-		log.Fatalf("error loading config: %v", err)
-	}
-
-	cfg := config.Client{}
-	err = k.Unmarshal("", &cfg)
+	app := client.NewCliApp()
+	err := app.StartProxy(context.Background())
 	if err != nil {
-		log.Fatalf("error unmarshaling config: %v", err)
-	}
-	config.SetClientDefaults(&cfg)
-
-	var privateKey *age.X25519Identity
-	if len(cfg.PrivateKey) == 0 {
-		privateKey, err = encoding.GenerateKey()
-		if err != nil {
-			log.Fatalf("error generating private key: %v", err)
-		}
-	} else {
-		privateKey, err = encoding.UnmarshalPrivateKey(cfg.PrivateKey)
-		if err != nil {
-			log.Fatalf("error unmarshaling private key from config: %v", err)
-		}
-	}
-
-	cfg.PrivateKey = privateKey.String()
-	fmt.Printf("My public key:\n%s\n", privateKey.Recipient().String())
-	// saving new values from defaults, generated private key
-	err = config.SaveConfig(cfg, config.ClientFilename)
-	if err != nil {
-		log.Fatalf("error saving config: %v", err)
-	}
-
-	serverPubKey, err := encoding.UnmarshalPublicKey(cfg.ServerPublicKey)
-	if err != nil {
-		log.Fatalf("error decoding server public key: %v", err)
-	}
-
-	proxy, err := socksproxy.NewClient(cfg.ProxyListenAddr)
-	if err != nil {
-		log.Fatalf("error setup proxy: %v", err)
+		log.Panicf("start proxy error: %v", err)
 	}
 	defer func() {
-		err := proxy.Close()
-		if err != nil {
-			log.Warnf("close proxy: %v", err)
-		}
+		app.StopProxy()
 	}()
 
-	encoder := encoding.NewEncoder(privateKey)
-	err = encoder.SetPeerPublicKey([]byte(serverPubKey.String()))
-	if err != nil {
-		log.Fatalf("error setting server public key: %v", err)
-	}
-
-	icqClient := icq.NewICQClient(cfg.ICQ.ClientToken)
-
-	// TODO: graceful shutdown
-	ctx, _ := context.WithCancel(context.Background())
-
-	msgCh := icqClient.MessageChan(ctx, cfg.ICQ.BotRoomID)
-	rwc := icq.NewRWCClient(ctx, icqClient, msgCh, &icq.ICQEncoder{Encoder: *encoder}, encoding.MaxMessageLen, cfg.ICQ.BotRoomID)
-
-	encKey, err := encoder.PackMessage(encoding.PublicKey, encoder.GetOwnPublicKey())
-	if err != nil {
-		panic(err)
-	}
-
-	err = icqClient.SendMessage(ctx, encKey, cfg.ICQ.BotRoomID)
-	if err != nil {
-		log.Fatalf("send public key error: %v", err)
-	}
-
-	yamuxCfg := yamux.DefaultConfig()
-	yamuxCfg.EnableKeepAlive = false
-	yamuxSession, err := yamux.Client(socksproxy.ConnWrapper{ReadWriteCloser: rwc}, yamuxCfg, nil)
-	if err != nil {
-		log.Fatalf("icq: can't start fething messages: %v", err)
-		return
-	}
-
-	proxyConns := proxy.ConnsChan()
-	for conn := range proxyConns {
-		stream, err := yamuxSession.Open(ctx)
-		if err != nil {
-			log.Warnf("open yamux session error: %v", err)
-			_ = conn.Close()
-			continue
-		}
-
-		go bidirectionalCopy(stream, conn)
-	}
-}
-
-func bidirectionalCopy(first io.ReadWriteCloser, second io.ReadWriteCloser) {
-	errCh := make(chan error, 2)
-	go func() {
-		_, err := io.Copy(first, second)
-		errCh <- err
-	}()
-	go func() {
-		_, err := io.Copy(second, first)
-		errCh <- err
-	}()
-
-	// Wait
-	for i := 0; i < 2; i++ {
-		err := <-errCh
-		if err != nil {
-			log.Warnf("proxy rwc conn error: %v", err)
-		}
-	}
-	_ = first.Close()
-	_ = second.Close()
+	quitCh := make(chan os.Signal, 1)
+	signal.Notify(quitCh, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	sig := <-quitCh
+	log.Infof("received exit signal '%s'", sig)
 }
--- go.mod
+++ go.mod
@@ -4,26 +4,42 @@ go 1.18
 
 require (
 	filippo.io/age v1.0.0
+	github.com/Kodeworks/golang-image-ico v0.0.0-20141118225523-73f0f4cfade9
+	github.com/getlantern/systray v1.2.1
 	github.com/haxii/socks5 v1.0.0
 	github.com/knadh/koanf v1.4.1
 	github.com/libp2p/go-yamux/v3 v3.1.1
 	github.com/mail-ru-im/bot-golang v0.0.0-20220405132937-fea9ed755353
+	github.com/ncruces/zenity v0.8.7
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.1
 	golang.org/x/net v0.0.0-20220517181318-183a9ca12b87
 )
 
 require (
+	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dchest/jsmin v0.0.0-20220218165748-59f39799265f // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/hako/durafmt v0.0.0-20190612201238-650ed9f29a84 // indirect
+	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
 	github.com/libp2p/go-buffer-pool v0.0.2 // indirect
 	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/image v0.0.0-20220413100746-70e8d0d3baa9 // indirect
+	golang.org/x/sys v0.0.0-20220502124256-b6088ccd6cba // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
